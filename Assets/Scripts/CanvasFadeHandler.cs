8df075f52fa7f9b2d504010420384f9afb5ad2ec
owlsketch <satimidus@gmail.com>
2020-04-24 12:19:46 -0400

    Menu UI in place, with fade animations and pointer

---
+++ Assets/Scripts/CanvasFadeHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasFadeHandler : MonoBehaviour
+{
+    public float duration = 0.4f;
+
+    public void FadeIn()
+    {
+        var canvGroup = GetComponent<CanvasGroup>();
+        StartCoroutine(ExecuteFade(canvGroup, canvGroup.alpha, 1));
+    }
+
+    public void FadeOut()
+    {
+        var canvGroup = GetComponent<CanvasGroup>();
+        StartCoroutine(ExecuteFade(canvGroup, canvGroup.alpha, 0));
+    }
+
+
+    public IEnumerator ExecuteFade(CanvasGroup group, float start, float end)
+    {
+        float timeElapsed = 0f;
+
+        while (timeElapsed < duration)
+        {
+            timeElapsed += Time.deltaTime;
+            group.alpha = Mathf.Lerp(start, end, timeElapsed / duration);
+
+            yield return null;
+        }
+
+    }
+}
