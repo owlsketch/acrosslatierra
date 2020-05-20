9db63eb0f51a70d3f99735913f2e768cb090cfb6
owlsketch <satimidus@gmail.com>
2020-05-20 13:47:30 -0400

    Joystick movement and star animation included

---
+++ Assets/Scripts/ImageFadeHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageFadeHandler : MonoBehaviour
+{
+    void Start()
+    {
+        RectTransform img = gameObject.GetComponent<RectTransform>();
+
+        LeanTween.alpha(img, 1f, 1.5f).setEase(LeanTweenType.easeInQuad).setDelay(2.75f);
+    }
+
+}
---
+++ Assets/Scripts/StarsFadeHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class StarsFadeHandler : MonoBehaviour
+{
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            RectTransform star = child.gameObject.GetComponent<RectTransform>();
+
+            LeanTween.alpha(star, 1f, 1.5f).setEase(LeanTweenType.easeInQuad).setDelay(Random.Range(0.0f, 1.5f));
+        }
+    }
+}
