46e69c5c9d3f276e15c265bb611632d609b217fa
owlsketch <satimidus@gmail.com>
2020-06-16 11:19:18 -0400

    Upgraded unity to 2019.3.15f1 - Upgraded to Universal Render Pipeline - Using Unity XR to manage VR controls - Implemented VR Camera Rig - Implemented basic UI Raycaster

--- Assets/Scripts/StartSceneTransition.cs
+++ Assets/Scripts/StartSceneTransition.cs
@@ -5,14 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class StartSceneTransition : MonoBehaviour
 {
-    public int minWaitTime = 3;
-
-    void Start() => StartCoroutine(LoadFireplace());
+    public void LoadNextScene() => StartCoroutine(LoadFireplace());
 
     IEnumerator LoadFireplace()
     {
-        yield return new WaitForSeconds(minWaitTime);
-
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Fireplace");
         while(!asyncLoad.isDone) { yield return null; }
     }
