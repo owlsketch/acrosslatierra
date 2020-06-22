b48c05a372a576dc666ee48ce9ebb0a612e1c061
owlsketch <satimidus@gmail.com>
2020-06-22 13:11:56 -0400

    Continuous movement, including slopes, done!

---
+++ Assets/Scripts/ContinuousMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ContinuousMovement : MonoBehaviour
+{
+    public XRNode inputSource;
+    public float speed = 1.0f;
+    public float gravity = -9.81f;
+    public LayerMask groundLayer;
+
+    private Vector2 inputAxis;
+    private float fallingSpeed;
+    private CharacterController character;
+    private XRRig rig;
+    
+
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        rig = GetComponent<XRRig>();
+    }
+    
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+    }
+
+    private void FixedUpdate()
+    {
+        CapsuleFollowHeadset();
+
+        Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
+        Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
+
+        character.Move(direction * Time.fixedDeltaTime * speed);
+
+        // v(t) = 0 if grounded, else v(t) = v0 + at.
+        bool isGrounded = DetermineIfGrounded();
+        if (isGrounded)
+            fallingSpeed = 0;
+        else 
+            fallingSpeed += gravity * Time.fixedDeltaTime;
+
+        character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
+    }
+
+    void CapsuleFollowHeadset()
+    {
+        // .2m fo yo big ass forehead
+        character.height = rig.cameraInRigSpaceHeight + 0.2f;
+
+        // transform camera pos from world space to local space
+        // need this to always place the capsule horizontally at camera pos
+        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
+        character.center = new Vector3(capsuleCenter.x, character.height/2 + character.skinWidth, capsuleCenter.z);
+    }
+
+    bool DetermineIfGrounded()
+    {
+        Vector3 rayStart = transform.TransformPoint(character.center);
+        float rayLength = character.center.y + 0.01f;
+
+        bool hasHit = Physics.SphereCast(rayStart, character.radius, Vector3.down, out RaycastHit hitInfo, rayLength, groundLayer);
+        return hasHit;
+    }
+}
